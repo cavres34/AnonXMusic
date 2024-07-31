47f40e7ed5b606833ac5e4dcf8bebefb255b03b6
Ahmetaffetmez <147531181+Ahmetaffetmez@users.noreply.github.com>
2024-07-31 07:32:36 +0300

    Update speed.py
    Signed-off-by: Ahmetaffetmez <147531181+Ahmetaffetmez@users.noreply.github.com>

--- AnonXMusic/plugins/admins/speed.py
+++ AnonXMusic/plugins/admins/speed.py
@@ -14,7 +14,7 @@ checker = []
 
 
 @app.on_message(
-    filters.command(["cspeed", "speed", "cslow", "slow", "playback", "cplayback"])
+    filters.command(["cspeed", "speed", "cslow", " hız", "slow", "playback", "cplayback"])
     & filters.group
     & ~BANNED_USERS
 )
